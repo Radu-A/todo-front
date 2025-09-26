995e5fbf888248d026c24db85302a468dd75d710
Radu-A <elcastravete@gmail.com>
2025-09-26 14:34:21 +0200

    added/new functionality

--- script/script.js
+++ script/script.js
@@ -1,12 +1,67 @@
+// VARIABLES
+
 const currentDate = document.getElementById("current-day");
 const today = new Date();
 
+// HEADER
+
+// get date to a variable
 const formatedDate = today.toLocaleDateString("en-EN", {
   weekday: "long",
   month: "long",
-  day: "numeric"
+  day: "numeric",
+});
+// insert date into DOM
+currentDate.textContent = formatedDate;
+
+// NEW TASK
+
+const newInput = document.getElementById("new-input");
+const newButton = document.getElementById("new-button");
+
+newInput.addEventListener("keydown", function (event) {
+  event.preventDefault;
+  if (event.key === "Enter") {
+    createNewTask(newInput.value);
+    newInput.value = "";
+  }
 });
 
-console.log(formatedDate);
+newButton.addEventListener("click", function (event) {
+  event.preventDefault;
+  createNewTask(newInput.value);
+  newInput.value = "";
+});
 
-currentDate.textContent = formatedDate;
+const createNewTask = (taskName) => {
+  const todoSection = document.getElementById("todo-section");
+  let taskArticle = document.createElement("article");
+  taskArticle.className = "todo-article";
+  taskArticle.id = taskName;
+  taskArticle.innerHTML = `
+		<div class="task-header">
+            <div class="status-icon pending"></div>
+            <h3 class="task-name">${taskName}</h3>
+		</div>`;
+  todoSection.appendChild(taskArticle);
+  asignEvent(taskName);
+};
+
+// STATUS ICON
+
+// no funciona!!!!!
+
+const asignEvent = (taskName) => {
+  const statusIcon = document.getElementById(taskName);
+  console.log(statusIcon);
+
+  statusIcon.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Holi");
+    if (statusIcon.className == "status-icon pending") {
+      statusIcon.className = "status-icon done";
+    } else {
+      statusIcon.className = "status-icon pending";
+    }
+  });
+};
