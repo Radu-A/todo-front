88fe983ba8632c84d6d2f76a116365d67407ed8f
Radu-A <elcastravete@gmail.com>
2025-09-29 13:08:29 +0200

    working/filter

--- script/script.js
+++ script/script.js
@@ -7,6 +7,11 @@ const doneSection = document.getElementById("done-section");
 const currentDate = document.getElementById("current-day");
 const newInput = document.getElementById("new-input");
 const newButton = document.getElementById("new-button");
+// const allButton = document.getElementById("all-filter");
+// const pendingButton = document.getElementById("pending-filter");
+// const completedButton = document.getElementById("completed-filter");
+const filterButtons = [...document.getElementsByClassName("filter-button")];
+console.log(filterButtons);
 
 // State on memory
 let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
@@ -15,11 +20,6 @@ console.log(taskList);
 // ==========================
 // UTILITY FUNCTIONS
 // ==========================
-
-// Save array of tasks on localStorage
-const saveTasks = () => {
-  localStorage.setItem("taskList", JSON.stringify(taskList));
-};
 
 // Formate current date and print on header
 const printCurrentDate = () => {
@@ -34,9 +34,104 @@ const printCurrentDate = () => {
   currentDate.textContent = formatedDate;
 };
 
+// Save array of tasks on localStorage
+const saveTasks = () => {
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+};
+
+// Charge all task saved on localStorage
+const loadTask = (state = "all") => {
+  let filteredTaskList = filterTasks(state);
+  filteredTaskList.forEach((task) => {
+    printTask(task.name, task.state);
+  });
+};
+
+// Clear tasks from DOM
+const clearTasks = () => {
+  todoSection.innerHTML = `
+        <div class="section-header">
+          <h2>ToDo</h2>
+        </div>`;
+  doneSection.innerHTML = `
+        <div class="section-header">
+          <h2>Done</h2>
+        </div>`;
+};
+
 // ==========================
 // TASKS FUNCTIONS
 // ==========================
+
+// Print task on apropiate section
+const printTask = (taskName, state) => {
+  let taskArticle = document.createElement("article");
+  taskArticle.className = "todo-article";
+  taskArticle.id = taskName;
+  taskArticle.innerHTML = `
+		<div class="task-header">
+            <div class="status-icon ${state}" id="${taskName}-icon"></div>
+            <h3 class="task-name">${taskName}</h3>
+		</div>
+		<button class="delete-button" id="${taskName}-delete-button"></button>`;
+  if (state == "todo") {
+    todoSection.appendChild(taskArticle);
+  } else {
+    doneSection.appendChild(taskArticle);
+  }
+  // Force reflow
+  requestAnimationFrame(() => {
+    taskArticle.classList.add("article-show");
+  });
+  activateDeleteButton(taskName, state);
+  asingState(taskArticle, taskName);
+};
+
+// Create a new task and save it
+const createTask = (taskName, state) => {
+  taskList.push({ name: taskName, state: state });
+  saveTasks();
+  printTask(taskName, state);
+};
+
+//  Asign event and state to a task
+const asingState = (taskArticle, taskName) => {
+  const stateIcon = document.getElementById(`${taskName}-icon`);
+
+  stateIcon.addEventListener("click", (event) => {
+    event.preventDefault();
+    let state = "todo";
+
+    if (stateIcon.classList.contains("todo")) {
+      state = "done";
+      taskArticle.classList.remove("article-show");
+      stateIcon.classList.remove("todo");
+      stateIcon.classList.add("done");
+      todoSection.removeChild(taskArticle);
+      doneSection.appendChild(taskArticle);
+      requestAnimationFrame(() => {
+        taskArticle.classList.add("article-show");
+      });
+    } else {
+      state = "todo";
+      taskArticle.classList.remove("article-show");
+      stateIcon.classList.remove("done");
+      stateIcon.classList.add("todo");
+      doneSection.removeChild(taskArticle);
+      todoSection.appendChild(taskArticle);
+      requestAnimationFrame(() => {
+        taskArticle.classList.add("article-show");
+      });
+    }
+
+    taskList.forEach((task) => {
+      if (task.name == taskName) {
+        task.state = state;
+      }
+    });
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  });
+};
 
 // Print delete-button on completed tasks
 const activateDeleteButton = (taskName, state) => {
@@ -67,69 +162,19 @@ const deleteFromDocument = (taskName, state) => {
   }
 };
 
-// Print task on apropiate section
-const printTask = (taskName, state) => {
-  let taskArticle = document.createElement("article");
-  taskArticle.className = "todo-article";
-  taskArticle.id = taskName;
-  taskArticle.innerHTML = `
-		<div class="task-header">
-            <div class="status-icon ${state}" id="${taskName}-icon"></div>
-            <h3 class="task-name">${taskName}</h3>
-		</div>
-		<button class="delete-button" id="${taskName}-delete-button">Delete</button>`;
-  if (state == "todo") {
-    todoSection.appendChild(taskArticle);
-  } else {
-    doneSection.appendChild(taskArticle);
-  }
-  activateDeleteButton(taskName, state);
-  asingState(taskArticle, taskName);
+// Filter taskList
+const filterTasks = (state = "all") => {
+  return state === "all"
+    ? taskList
+    : taskList.filter((task) => task.state == state);
 };
 
-// Create a new task and save it
-const createTask = (taskName, state) => {
-  taskList.push({ name: taskName, state: state });
-  saveTasks();
-  printTask(taskName, state);
-};
-
-//  Asign event and state to a task
-const asingState = (taskArticle, taskName) => {
-  const stateIcon = document.getElementById(`${taskName}-icon`);
-
-  stateIcon.addEventListener("click", (event) => {
-    event.preventDefault();
-    let state = "todo";
-
-    if (stateIcon.classList.contains("todo")) {
-      state = "done";
-      stateIcon.classList.remove("todo");
-      stateIcon.classList.add("done");
-      todoSection.removeChild(taskArticle);
-      doneSection.appendChild(taskArticle);
-      activateDeleteButton(taskName);
-    } else {
-      state = "todo";
-      stateIcon.classList.remove("done");
-      stateIcon.classList.add("todo");
-      doneSection.removeChild(taskArticle);
-      todoSection.appendChild(taskArticle);
-    }
-
-    taskList.forEach((task) => {
-      if (task.name == taskName) {
-        task.state = state;
-      }
-    });
-    localStorage.setItem("taskList", JSON.stringify(taskList));
-  });
-};
-
-// Charge all task saved on localStorage
-const loadTask = () => {
-  taskList.forEach((task) => {
-    printTask(task.name, task.state);
+// Activate filter button
+const activateFilterButton = (clickedButton) => {
+  filterButtons.forEach((button) => {
+    button == clickedButton
+      ? button.classList.add("filter-active")
+      : button.classList.remove("filter-active");
   });
 };
 
@@ -148,6 +193,15 @@ newButton.addEventListener("click", (event) => {
   event.preventDefault();
   createTask(newInput.value, "todo");
   newInput.value = "";
+});
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    clearTasks();
+    loadTask(button.dataset.filter);
+    activateFilterButton(button);
+  });
 });
 
 // ==========================
