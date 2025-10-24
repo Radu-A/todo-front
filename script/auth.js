70c4c93638af99540f50ffca656c7cb6b60c628e
Radu-A <elcastravete@gmail.com>
2025-10-24 17:29:30 +0200

    created/auth.js

---
+++ script/auth.js
@@ -0,0 +1,10 @@
+// ==================
+// VARIABLES
+// ==================
+const emailLoginInput = document.getElementById("email-login-input");
+const passwordLoginInput = document.getElementById("password-login-input");
+console.log(emailLoginInput, passwordLoginInput);
+
+// ==================
+// VALIDATION
+// ==================
